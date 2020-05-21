13ca21b9dc439740f5407395060e75fe19b884ce
Adrian <spyip3@student.monash.edu>
2020-05-21 15:21:52 +1000

    Added 3 register fragments

---
+++ app/src/main/java/com/example/mymoviemenoir/fragment/register_p2_fragment.java
@@ -0,0 +1,30 @@
+package com.example.mymoviemenoir.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mymoviemenoir.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class register_p2_fragment extends Fragment {
+
+    public register_p2_fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register_p2, container, false);
+    }
+}
