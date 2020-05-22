76ff03c5b1cabce2dfe22596b4b28080a6e1c394
Adrian <spyip3@student.monash.edu>
2020-05-22 16:34:49 +1000

    Designed search, watchlist and home layout

---
+++ app/src/main/java/com/example/mymoviemenoir/AddToMemoirActivity.java
@@ -0,0 +1,14 @@
+package com.example.mymoviemenoir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddToMemoirActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_memoir);
+    }
+}
